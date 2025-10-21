25ed056e712adb1b9f2925b9da576cc71937b35c
daw2-servian26 <proytjoselo2@gmail.com>
2025-10-21 16:48:28 +0200

    Versió 0.2-A del projecte prjava43servian26. Branca00 creada i modificada

--- src/prjava43servian26/Prjava43servian26.java
+++ src/prjava43servian26/Prjava43servian26.java
@@ -38,8 +38,8 @@ public Prjava43servian26 () {
         // "this" Frame fires WindowEvent its registered WindowEvent listener 
         // "this" Frame adds "this" object as a WindowEvent listener 
  
-      setTitle("prjava43servian26");  // "this" Frame sets title 
-      setSize(250, 100);        // "this" Frame sets initial size 
+      setTitle("nova finestra prjava43servian26");  // "this" Frame sets title 
+      setSize(450, 100);        // "this" Frame sets initial size 
       setVisible(true);         // "this" Frame shows 
    } 
  
